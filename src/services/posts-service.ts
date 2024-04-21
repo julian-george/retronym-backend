1ed0c9dcbadb33c35e1a3946b68c17a7a697a069
miruna <58366207+mirunaish@users.noreply.github.com>
2024-04-21 05:54:51 -0400

    added reddit, youtube

--- src/services/posts-service.ts
+++ src/services/posts-service.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { getAccessTokens } from "./user-service";
 import User from "../models/User";
-import { isNull, isUndefined } from "lodash";
+import { isNull, isUndefined, sample } from "lodash";
 
-const POSTS_PER_REQUEST = 1;
+const POSTS_PER_REQUEST = "1";
 
 export async function getPosts(userId: string) {
   try {
@@ -34,19 +34,28 @@ export async function getPosts(userId: string) {
         "https://api.twitter.com/2/users/" +
           user.twitterId +
           "/timelines/reverse_chronological?tweet.fields=text&expansions=author_id&max_results=" +
-          POSTS_PER_REQUEST.toString(),
+          POSTS_PER_REQUEST,
         { headers: { Authorization: `Bearer ${user.twitterToken}` } }
       )
     ).data;
     posts.twitterPosts = twitterPosts;
 
-    // const redditPosts = (await axios.get("https://api.reddit.com/best")).data;
-    // posts.redditPosts = redditPosts;
+    const redditPosts = (
+      await axios.get("https://api.reddit.com/best?limit=" + POSTS_PER_REQUEST)
+    ).data;
+    posts.redditPosts = redditPosts;
 
-    // const youtubePosts = (
-    //   await axios.get("https://www.googleapis.com/youtube/v3/search")
-    // ).data;
-    // posts.youtubePosts = youtubePosts;
+    const query = sample(user.preferences.searchTerms);
+    const youtubePosts = (
+      await axios.get(
+        "https://www.googleapis.com/youtube/v3/search?q=" +
+          query +
+          "&type=video&videoDuration=short&videoEmbeddable=true&safeSearch=strict&maxResults=" +
+          POSTS_PER_REQUEST,
+        { headers: { Authorization: `Bearer ${user.youtubeToken}` } }
+      )
+    ).data;
+    posts.youtubePosts = youtubePosts;
 
     return { success: true, posts };
   } catch (error: any) {
